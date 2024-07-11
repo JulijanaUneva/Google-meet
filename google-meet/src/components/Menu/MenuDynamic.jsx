2d268485e07897677c593779ddab1cc79d599b47
JianLu <jian.lu@dci-student.org>
2024-07-11 11:06:36 +0200

    feat: Add react-router-dom for routing in Google Meet app

--- google-meet/src/components/Menu/MenuDynamic.jsx
+++ google-meet/src/components/Menu/MenuDynamic.jsx
@@ -1,29 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInfoCircle, faUser, faCommentAlt, faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from "react-router-dom";
 
-// export const useToggle = () => {
-//     const [onOff, setOnOff] = useState(
-//         {
-//             audioSet: false,
-//             microphone: false,
-//             videoSet: false,
-//             camera: false,
-//             captions: false,
-//             reaction: false,
-//             present: false,
-//             raiseHand: false,
-//             moreOptions: false,
-//             leave: false
-//         }
-//     )
-//     const toggle = () => setOnOff(!{ ...onOff })
-//     return { onOff, toggle }
-// }
 
 const MenuDynamic = () => {
-    // const { onOff, toggle } = useToggle()
+    const navigate = useNavigate()
     const [onOff, setOnOff] = useState(
         {
             audioSet: false,
@@ -38,11 +21,20 @@ const MenuDynamic = () => {
             leave: false
         })
 
+    // 你遇到的警告是因为在渲染过程中调用了setState，这会导致组件在同一渲染周期内再次渲染。具体来说，MenuDynamic组件在渲染过程中调用了navigate函数，这会尝试更新BrowserRouter组件的状态，从而导致这个警告。
+    // 要解决这个问题，你可以使用useEffect钩子来处理导航逻辑，而不是直接在渲染过程中调用navigate。
+    useEffect(() => {
+        if (onOff.leave) {
+            navigate('/leave');
+        }
+    }, [onOff.leave, navigate]);
+
+
     return (<>
 
+        {/* {onOff.leave && navigate('/leave')} */}
 
         <div style={{ display: 'flex', justifyContent: 'space-between', flexBasis: '66%' }}>
-
 
             {onOff.audioSet &&
                 <div className="audio-settings-container">
@@ -96,8 +88,8 @@ const MenuDynamic = () => {
                     <p onClick={() => setOnOff({ ...onOff, raiseHand: !(onOff.raiseHand) })} style={{ backgroundColor: onOff.raiseHand && 'blue' }}><svg className="center-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="26px"><path d="M256 0c-25.3 0-47.2 14.7-57.6 36c-7-2.6-14.5-4-22.4-4c-35.3 0-64 28.7-64 64V261.5l-2.7-2.7c-25-25-65.5-25-90.5 0s-25 65.5 0 90.5L106.5 437c48 48 113.1 75 181 75H296h8c1.5 0 3-.1 4.5-.4c91.7-6.2 165-79.4 171.1-171.1c.3-1.5 .4-3 .4-4.5V160c0-35.3-28.7-64-64-64c-5.5 0-10.9 .7-16 2V96c0-35.3-28.7-64-64-64c-7.9 0-15.4 1.4-22.4 4C303.2 14.7 281.3 0 256 0zM240 96.1c0 0 0-.1 0-.1V64c0-8.8 7.2-16 16-16s16 7.2 16 16V95.9c0 0 0 .1 0 .1V232c0 13.3 10.7 24 24 24s24-10.7 24-24V96c0 0 0 0 0-.1c0-8.8 7.2-16 16-16s16 7.2 16 16v55.9c0 0 0 .1 0 .1v80c0 13.3 10.7 24 24 24s24-10.7 24-24V160.1c0 0 0-.1 0-.1c0-8.8 7.2-16 16-16s16 7.2 16 16V332.9c-.1 .6-.1 1.3-.2 1.9c-3.4 69.7-59.3 125.6-129 129c-.6 0-1.3 .1-1.9 .2H296h-8.5c-55.2 0-108.1-21.9-147.1-60.9L52.7 315.3c-6.2-6.2-6.2-16.4 0-22.6s16.4-6.2 22.6 0L119 336.4c6.9 6.9 17.2 8.9 26.2 5.2s14.8-12.5 14.8-22.2V96c0-8.8 7.2-16 16-16c8.8 0 16 7.1 16 15.9V232c0 13.3 10.7 24 24 24s24-10.7 24-24V96.1z" /></svg></p>
 
                     <p><svg className="center-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 512" width="6px"><path d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z" /></svg></p>
-
-                    <p><svg className="center-svg " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" width="26px"><path d="M601.2 5.1c10.4-8.2 25.5-6.3 33.7 4.1s6.3 25.5-4.1 33.7l-592 464c-10.4 8.2-25.5 6.3-33.7-4.1s-6.3-25.5 4.1-33.7l155.6-122C101.8 270 64 171.4 64 64c0-18 12.1-33.8 29.5-38.6l88-24c19.4-5.3 39.7 4.6 47.4 23.2l40 96c6.8 16.3 2.1 35.2-11.6 46.3L208 207.3c12 25.5 27.2 49.2 45 70.7L601.2 5.1zM234.3 415.6l91.3-72c13.7 9.1 28 17.3 43 24.3L409 318.7c11.2-13.7 30-18.4 46.3-11.6l96 40c18.6 7.7 28.5 28 23.2 47.4l-24 88C545.8 499.9 530 512 512 512c-104.9 0-201.3-36-277.7-96.4z" /></svg></p>
+                    {/* leave */}
+                    <p onClick={() => setOnOff({ ...onOff, leave: !(onOff.leave) })}><svg className="center-svg " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" width="26px"><path d="M601.2 5.1c10.4-8.2 25.5-6.3 33.7 4.1s6.3 25.5-4.1 33.7l-592 464c-10.4 8.2-25.5 6.3-33.7-4.1s-6.3-25.5 4.1-33.7l155.6-122C101.8 270 64 171.4 64 64c0-18 12.1-33.8 29.5-38.6l88-24c19.4-5.3 39.7 4.6 47.4 23.2l40 96c6.8 16.3 2.1 35.2-11.6 46.3L208 207.3c12 25.5 27.2 49.2 45 70.7L601.2 5.1zM234.3 415.6l91.3-72c13.7 9.1 28 17.3 43 24.3L409 318.7c11.2-13.7 30-18.4 46.3-11.6l96 40c18.6 7.7 28.5 28 23.2 47.4l-24 88C545.8 499.9 530 512 512 512c-104.9 0-201.3-36-277.7-96.4z" /></svg></p>
                 </div>
 
 
--- google-meet/src/main.jsx
+++ google-meet/src/main.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import LeaveMeet from './pages/LeaveMeet.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+
+    <BrowserRouter>
+
+
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/leave' element={<LeaveMeet />} />
+
+      </Routes>
+    </BrowserRouter>
+    {/* <App /> */}
   </React.StrictMode>,
 )
