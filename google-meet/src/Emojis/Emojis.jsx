36f8381450513ffe1cebb48c51c862d944ef21f0
Alex <mohammad.mohammad@dci-student.org>
2024-07-11 14:17:30 +0200

    Emojis-clicked

--- google-meet/src/Emojis/Emojis.jsx
+++ google-meet/src/Emojis/Emojis.jsx
@@ -1,9 +1,39 @@
-import React from 'react';
-import "./Emojis.css";
+import React, {useState} from 'react';
+
+import "./Emojis.css"; 
+
 const Emojis = () => {
+   
+   const [emoji, setEmoji] = useState("")
+   const [skin, setSkin] = useState("")
+//    
     return (
+        <div>
         <div className='emojis'>
-            💖 👍 🎉 👏  😂 😮 😢 🤔 👎
+            <h1 style={{position:"relative",top:"-75px",left:"-400px", fontSize:"60px"}}>
+                {emoji}</h1>
+            <h5 className='skinText'>{skin}</h5>
+            <button onClick={()=>{setEmoji("💖")}} >💖</button>
+            <button onClick={()=>{setEmoji("👍")}} >👍</button>
+            <button onClick={()=>{setEmoji("🎉")}} >🎉</button>
+            <button onClick={()=>{setEmoji("👏")}} >👏</button>
+            <button onClick={()=>{setEmoji("😂")}} >😂</button>
+            <button onClick={()=>{setEmoji("😮")}} >😮</button>
+            <button onClick={()=>{setEmoji("😢")}} >😢</button>
+            <button onClick={()=>{setEmoji("🤔")}} >🤔</button>
+            <button onClick={()=>{setEmoji("👎")}} >👎</button>
+            
+            {/*
+             <span style={{fontSize:"25px"}}>👍</span>
+            <span style={{fontSize:"25px"}}>🎉</span>
+            <span style={{fontSize:"25px"}}>👏</span>
+            <span style={{fontSize:"25px"}}>😂</span>
+            <span style={{fontSize:"25px"}}>😮</span>
+            <span style={{fontSize:"25px"}}>😢</span> 
+            <span style={{fontSize:"25px"}}>🤔</span> 
+            <span style={{fontSize:"25px"}}>👎</span>      */}
+        </div>
+        {/* <button className='skin' onClick={()=>{setSkin("Skin tone")} }  >😶</button> */}
         </div>
     );
 }
