5bd71ca11f7904d4cfdafa855461725b875bc4ae
JianLu <jian.lu@dci-student.org>
2024-07-11 19:32:09 +0200

    chang the render condition

--- google-meet/src/Emojis/Emojis.jsx
+++ google-meet/src/Emojis/Emojis.jsx
@@ -5,23 +5,25 @@ import { useState } from 'react';
 const Emojis = () => {
 
     const [emoji, setEmoji] = useState("")
-    const [animateKey, setAnimateKey] = useState(0);
+    // const [animateKey, setAnimateKey] = useState(0);
     //    const [skin, setSkin] = useState("")
 
     return (
         <div>
             <div className='emojis'>
-                {emoji && <h1 key={animateKey} className='emoji-clicked' onAnimationEnd={() => setEmoji("")} >{emoji}</h1>}
+                {/* {emoji && <h1 key={animateKey} className='emoji-clicked' onAnimationEnd={() => setEmoji("")} >{emoji}</h1>} */}
+                {emoji && <h1 className='emoji-clicked' onAnimationEnd={() => setEmoji("")} >{emoji}</h1>}
                 {/* <h5 className='skinText'>{skin}</h5> */}
-                <button onClick={() => { setEmoji("💖"); setAnimateKey(animateKey + 1) }} >💖</button>
-                <button onClick={() => { setEmoji("👍"); setAnimateKey(animateKey + 1) }} >👍</button>
-                <button onClick={() => { setEmoji("🎉"); setAnimateKey(animateKey + 1) }} >🎉</button>
-                <button onClick={() => { setEmoji("👏"); setAnimateKey(animateKey + 1) }} >👏</button>
-                <button onClick={() => { setEmoji("😂"); setAnimateKey(animateKey + 1) }} >😂</button>
-                <button onClick={() => { setEmoji("😮"); setAnimateKey(animateKey + 1) }} >😮</button>
-                <button onClick={() => { setEmoji("😢"); setAnimateKey(animateKey + 1) }} >😢</button>
-                <button onClick={() => { setEmoji("🤔"); setAnimateKey(animateKey + 1) }} >🤔</button>
-                <button onClick={() => { setEmoji("👎"); setAnimateKey(animateKey + 1) }} >👎</button>
+                <button onClick={() => { setEmoji("💖"); }} >💖</button>
+                <button onClick={() => { setEmoji("👍"); }} >👍</button>
+                <button onClick={() => { setEmoji("🎉"); }} >🎉</button>
+                <button onClick={() => { setEmoji("👏"); }} >👏</button>
+                <button onClick={() => { setEmoji("😂"); }} >😂</button>
+                <button onClick={() => { setEmoji("😮"); }} >😮</button>
+                <button onClick={() => { setEmoji("😢"); }} >😢</button>
+                <button onClick={() => { setEmoji("🤔"); }} >🤔</button>
+                <button onClick={() => { setEmoji("👎"); }} >👎</button>
+                {/* <button onClick={() => { setEmoji("👎"); setAnimateKey(animateKey + 1) }} >👎</button> */}
 
 
             </div>
