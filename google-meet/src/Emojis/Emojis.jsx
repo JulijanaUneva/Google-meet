395430a4bec1de8e6323cc17c7805794f3cca54b
Alex <mohammad.mohammad@dci-student.org>
2024-07-11 16:44:40 +0200

    emojiMove

--- google-meet/src/Emojis/Emojis.jsx
+++ google-meet/src/Emojis/Emojis.jsx
@@ -1,3 +1,12 @@
+// module.exports = {
+//     content: [
+//       './src/**/*.{html,js,jsx,ts,tsx}', // Sucht in diesen Verzeichnissen nach Tailwind-Klassen
+//     ],
+//     theme: {
+//       extend: {},
+//     },
+//     plugins: [],
+//   }
 import React, {useState} from 'react';
 
 import "./Emojis.css"; 
@@ -5,14 +14,13 @@ import "./Emojis.css";
 const Emojis = () => {
    
    const [emoji, setEmoji] = useState("")
-   const [skin, setSkin] = useState("")
+//    const [skin, setSkin] = useState("")
 //    
     return (
         <div>
         <div className='emojis'>
-            <h1 style={{position:"relative",top:"-75px",left:"-400px", fontSize:"60px"}}>
-                {emoji}</h1>
-            <h5 className='skinText'>{skin}</h5>
+            <h1 className='emoji-clicked'>{emoji}</h1>
+            {/* <h5 className='skinText'>{skin}</h5> */}
             <button onClick={()=>{setEmoji("💖")}} >💖</button>
             <button onClick={()=>{setEmoji("👍")}} >👍</button>
             <button onClick={()=>{setEmoji("🎉")}} >🎉</button>
@@ -23,15 +31,7 @@ const Emojis = () => {
             <button onClick={()=>{setEmoji("🤔")}} >🤔</button>
             <button onClick={()=>{setEmoji("👎")}} >👎</button>
             
-            {/*
-             <span style={{fontSize:"25px"}}>👍</span>
-            <span style={{fontSize:"25px"}}>🎉</span>
-            <span style={{fontSize:"25px"}}>👏</span>
-            <span style={{fontSize:"25px"}}>😂</span>
-            <span style={{fontSize:"25px"}}>😮</span>
-            <span style={{fontSize:"25px"}}>😢</span> 
-            <span style={{fontSize:"25px"}}>🤔</span> 
-            <span style={{fontSize:"25px"}}>👎</span>      */}
+           
         </div>
         {/* <button className='skin' onClick={()=>{setSkin("Skin tone")} }  >😶</button> */}
         </div>
